f51cc604e8493ce0b1516929868fa72637a98694
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-15 23:17:33 +0530

    add Alternative Square Pattern

---
+++ LEARNDSA/CCSTART2/alternative_square_pattern.py
@@ -0,0 +1,15 @@
+N = int(input())
+k = -4
+for i in range(1,N+1):
+    if i%2!=0:
+        k+=4
+        for j in range(1,6):
+            k+=1
+            print(k,end=" ")
+        print("")
+    else:
+        k+=6
+        for j in range(1,6):
+            k-=1
+            print(k,end=" ")
+        print("")
