4dd9a5b7040b7e5eaab491946f2710cde8a289fe
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-17 22:26:44 +0530

    add Smartphone

--- LEARNDSA/DSA Learning Series - Contest 1/smartphone.py
+++ LEARNDSA/DSA Learning Series - Contest 1/smartphone.py
@@ -1,1 +1,14 @@
 '''Link to problem: https://www.codechef.com/LRNDSA01/problems/ZCO14003'''
+
+N = int(input())
+array = []
+for i in range(N):
+    budget = int(input())
+    array.append(budget)
+
+array.sort()
+ansArray = []
+for i in range(len(array)):
+    #x = array[i]*(N-i)
+    ansArray.append(array[i]*(N-i))
+print(max(ansArray))
