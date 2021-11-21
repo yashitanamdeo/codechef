4f291994f720240f03870a86a1e869eee154c364
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-21 19:47:16 +0530

    add Carvans

--- LEARNDSA/DSA Learning Series - Contest 1/carvans.py
+++ LEARNDSA/DSA Learning Series - Contest 1/carvans.py
@@ -1,1 +1,13 @@
 """Problem Statement: https://www.codechef.com/problems/CARVANS"""
+
+for _ in range(int(input())):
+    N = int(input())
+    arr = [int(i) for i in input().split()]
+    
+    count = 0
+    Min = arr[0]
+    for i in range(N):
+        if arr[i] <= Min:
+            Min = min(Min,arr[i])
+            count += 1
+    print(count)
