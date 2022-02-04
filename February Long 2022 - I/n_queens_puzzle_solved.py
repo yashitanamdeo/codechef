7971c4d6369cf848a0e5322e20f25dde0e1474f5
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-04 19:56:32 +0530

    add N Queens Puzzle Solved!

--- February Long 2022 - I/n_queens_puzzle_solved.py
+++ February Long 2022 - I/n_queens_puzzle_solved.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/FEB221C/problems/EUREKA
+
+
+import math
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    value = (0.143 * N) ** N
+    if (value - math.floor(value)) < 0.5:
+        print(math.floor(value))
+    else:
+        print(math.floor(value)+1)
