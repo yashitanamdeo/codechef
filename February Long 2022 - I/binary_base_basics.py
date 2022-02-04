3d4e9fa52cc15ff89cf79939d58c4632639eb076
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-04 21:26:36 +0530

    add Binary Base Basics

--- February Long 2022 - I/binary_base_basics.py
+++ February Long 2022 - I/binary_base_basics.py
@@ -1,1 +1,26 @@
 # Problem Statement: https://www.codechef.com/FEB221C/problems/BINBASBASIC
+
+# cook your dish here
+T = int(input())
+for _ in range(T):
+    N,K = map(int,input().split())
+    S = str(input())
+    
+    i = 0
+    j = N - 1
+    notEqualCount = 0
+    while(i<j):
+        if S[i] != S[j]:
+            notEqualCount += 1
+        i += 1
+        j -= 1        
+    
+    if notEqualCount <= K:
+        if N%2 == 1:
+            print("YES")
+        elif (K-notEqualCount)%2 == 0:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
