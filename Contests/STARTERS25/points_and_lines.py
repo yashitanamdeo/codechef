abf3b4df0df7eaeb1e3b1e75964966c8c00dc51f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-09 22:51:31 +0530

    add Points and Lines

--- Contests/STARTERS25/points_and_lines.py
+++ Contests/STARTERS25/points_and_lines.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/START25C/problems/POLIN
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    Xpoints,Ypoints = [],[]
+    for i in range(N):
+        Xi,Yi = map(int,input().split())
+        Xpoints.append(Xi)
+        Ypoints.append(Yi)
+    
+    print(len(set(Xpoints)) + len(set(Ypoints)))
