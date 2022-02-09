17217e67fdf562dda482528c67bc5c7098e500bc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-09 21:54:40 +0530

    add Chef and his Fruit Stand

--- Contests/STARTERS25/chef_and_his_fruit_stand.py
+++ Contests/STARTERS25/chef_and_his_fruit_stand.py
@@ -1,1 +1,10 @@
 # Problem Statement: https://www.codechef.com/START25C/problems/FRUITCHAAT
+
+T = int(input())
+for _ in range(T):
+    X,Y = map(int,input().split())
+    banana = X//2
+    if Y > banana:
+        print(banana)
+    else:
+        print(Y)
