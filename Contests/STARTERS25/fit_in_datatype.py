c9198b5b0ae7b94611809e9f3f3954639d3c579a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-09 22:49:06 +0530

    add Fit in Datatype

--- Contests/STARTERS25/fit_in_datatype.py
+++ Contests/STARTERS25/fit_in_datatype.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://www.codechef.com/START25C/problems/DATATYPE
+
+T = int(input())
+for _ in range(T):
+    N,X = map(int,input().split())
+    if X <= N:
+        print(X)
+    else:
+        while(X > N):
+            X = X-N-1
+        print(X)
