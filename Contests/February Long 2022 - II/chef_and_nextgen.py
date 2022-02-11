263c5883df9266e0fd51c1a7e7350275361e6b23
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-12 00:00:17 +0530

    add Chef and NextGen

--- Contests/February Long 2022 - II/chef_and_nextgen.py
+++ Contests/February Long 2022 - II/chef_and_nextgen.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://www.codechef.com/FEB222C/problems/HELIUM3
+
+T = int(input())
+for _ in range(T):
+    A,B,X,Y = map(int,input().split())
+    if X*Y >= A*B:
+        print("YES")
+    else:
+        print("NO")
