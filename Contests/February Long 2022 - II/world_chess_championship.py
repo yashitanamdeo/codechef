d94f0d79aa850794e08b9d884b4ffa41983714ce
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-12 00:03:48 +0530

    add World Chess Championship

--- Contests/February Long 2022 - II/world_chess_championship.py
+++ Contests/February Long 2022 - II/world_chess_championship.py
@@ -1,1 +1,24 @@
 # Problem Statement: https://www.codechef.com/FEB222C/problems/WCC/
+
+T = int(input())
+for _ in range(T):
+    X = int(input())
+    game = input()
+    Ccount,Ncount,Dcount = 0,0,0
+    for i in range(14):
+        if game[i] == 'C':
+            Ccount += 1
+        if game[i] == 'N':
+            Ncount += 1
+        if game[i] == 'D':
+            Dcount += 1
+    
+    Cpoints = 2 * Ccount + (1 * Dcount)
+    Npoints = 2 * Ncount + (1 * Dcount)
+
+    if Cpoints > Npoints:
+        print(60 * X)
+    elif Npoints > Cpoints:
+        print(40 * X)
+    else:
+        print(55 * X)
