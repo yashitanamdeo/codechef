636f4a6b42bdaf54efb0607c60aa5bb9842bb118
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-12 00:09:19 +0530

    add Avoid Fixed Points

--- Contests/February Long 2022 - II/avoid_fixed_points.py
+++ Contests/February Long 2022 - II/avoid_fixed_points.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/FEB222C/problems/NOFIX
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int,input().split()))
+    count = 0
+    for i in range(N):
+        value = i+1+count
+        if arr[i] == value:
+            count += 1
+    print(count)
