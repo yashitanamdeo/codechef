2d5fb582a90b373a38058917bb2bda521ad82bae
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-16 23:37:00 +0530

    add Hostel Room

--- Contests/STARTERS26/hostel_room.py
+++ Contests/STARTERS26/hostel_room.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/START26C/problems/HOSTELROOM
+
+T=int(input())
+for _ in range(T):
+    N,X = map(int,input().split())
+    A = list(map(int,input().split()))
+    Sum = X
+    maximumSum = X
+    for num in A:
+        Sum += num
+        maximumSum = max(Sum,maximumSum)
+    print(maximumSum)
