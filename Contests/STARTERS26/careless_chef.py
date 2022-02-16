819887390599363a1da4f4c47846e8d4c3f7af17
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-16 23:38:36 +0530

    add Careless Chef

--- Contests/STARTERS26/careless_chef.py
+++ Contests/STARTERS26/careless_chef.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://www.codechef.com/START26C/problems/LOSTSEQ
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    B = list(map(int,input().split()))
+    add = 0
+    for i in range(len(B)):
+        add += B[i]
+    if add % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
