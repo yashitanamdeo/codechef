1188071f0e31e57eda1a9c5fc41623af756603fe
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-23 23:21:42 +0530

    add Distinct Binary Strings

--- Contests/STARTERS27/distinct_binary_strings.py
+++ Contests/STARTERS27/distinct_binary_strings.py
@@ -1,1 +1,30 @@
 # Problem Statement: https://www.codechef.com/START27C/problems/BINSTRING
+
+# Accepted
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    S = input()
+    count = N
+
+    for i in range(1,N):
+        if S[i] == S[i-1]:
+            count -= 1
+    print(count)
+
+# RTE(other)
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    S = input()
+    memory = []
+    new = ''
+
+    for i in range(N):
+        new = S[:i] + S[i+1:]
+        memory.append(new)
+
+    memory_set = set(memory)
+    #print(memory)
+    #print(memory_set)
+    print(len(memory_set))
