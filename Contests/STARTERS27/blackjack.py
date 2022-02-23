ca53424fb4d314c43fd761458e5f9a5c1383219a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-23 23:14:47 +0530

    add Blackjack

--- Contests/STARTERS27/blackjack.py
+++ Contests/STARTERS27/blackjack.py
@@ -1,1 +1,11 @@
 # Problem Statement: https://www.codechef.com/START27C/problems/BLACKJACK
+
+T = int(input())
+for _ in range(T):
+    A,B = map(int,input().split())
+    a_plus_b = A + B
+    balance = 21 - a_plus_b
+    if balance <= 10:
+        print(balance)
+    else:
+        print(-1)
