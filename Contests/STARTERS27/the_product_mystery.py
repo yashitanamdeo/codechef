84ec927045fdb281e548f48b870880d229eb2ffe
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-02-23 23:18:47 +0530

    add The product mystery

--- Contests/STARTERS27/the_product_mystery.py
+++ Contests/STARTERS27/the_product_mystery.py
@@ -1,1 +1,7 @@
 # Problem Statement: https://www.codechef.com/START27C/problems/PRODUCT
+
+import math
+T = int(input())
+for _ in range(T):
+    B,C = map(int,input().split())
+    print(C//math.gcd(B,C))
