d443297aa485bb4e1c573d8b00c15bd5db1b5396
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-01 23:38:41 +0530

    add Good Program

--- Contests/February Lunchtime/good_program.py
+++ Contests/February Lunchtime/good_program.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://www.codechef.com/LTIME105C/problems/NIBBLE
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    if N%4 == 0:
+        print("Good")
+    else:
+        print("Not Good")
