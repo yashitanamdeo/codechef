641c027968e0e45dadbdb50268cbb6f2626a7df6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-01 23:44:59 +0530

    add Parallel Processing

--- Contests/February Lunchtime/parallel_processing.py
+++ Contests/February Lunchtime/parallel_processing.py
@@ -1,1 +1,17 @@
 # Problem Statement: https://www.codechef.com/LTIME105C/problems/PLPROCESS
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int,input().split()))
+    sumA = 0
+    array = [0] * N
+    
+    for i in range(N):
+        sumA += A[i]
+        array[i] = sumA
+    
+    answer = sumA
+    for i in range(N):
+        answer = min(max(array[i], sumA - array[i]), answer)
+    print(answer)
