b602e2c12d9cb7fd3a4d21e1c7a7cfc25afa0d8d
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-02 21:52:26 +0530

    add Car choice

--- Contests/STARTERS28/car_choice.py
+++ Contests/STARTERS28/car_choice.py
@@ -1,1 +1,15 @@
 # Problem Statement: https://www.codechef.com/START28C/problems/CARCHOICE
+
+T = int(input())
+for _ in range(T):
+    x1,x2,y1,y2 = map(int,input().split())
+    
+    c1_cost = y1/x1
+    c2_cost = y2/x2
+    
+    if c1_cost == c2_cost:
+        print("0")
+    elif c1_cost < c2_cost:
+        print("-1")
+    else:
+        print("1")
