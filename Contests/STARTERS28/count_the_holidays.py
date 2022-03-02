a2ce9ef7d965b0d204b632156cf2029a65801b52
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-02 21:55:40 +0530

    add Count the Holidays

--- Contests/STARTERS28/count_the_holidays.py
+++ Contests/STARTERS28/count_the_holidays.py
@@ -1,1 +1,14 @@
 # Problem Statement: https://www.codechef.com/START28C/problems/SUNDAY
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int,input().split()))
+    
+    holidays = [6,7,13,14,20,21,27,28]
+    total_holidays = 8
+    for i in range(N):
+        if A[i] not in holidays:
+            total_holidays += 1
+    
+    print(total_holidays)
