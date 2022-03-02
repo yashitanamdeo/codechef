1a808817ccbe1603e5a49cd5321cc8db5f07e25e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-02 21:50:57 +0530

    add Chef Defence Training

--- Contests/STARTERS28/chef_defence_training.py
+++ Contests/STARTERS28/chef_defence_training.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/START28C/problems/SELFDEF
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = list(map(int,input().split()))
+    
+    count = 0
+    for i in range(N):
+        if arr[i] in range(10,61):
+            count += 1
+    print(count)
