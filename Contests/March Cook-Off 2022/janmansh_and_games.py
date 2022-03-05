f34ead911ff3b609b38b51487e6473c6d5b23685
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-06 00:14:05 +0530

    add Janmansh and Games

--- Contests/March Cook-Off 2022/janmansh_and_games.py
+++ Contests/March Cook-Off 2022/janmansh_and_games.py
@@ -1,1 +1,9 @@
 # Problem Statement: https://www.codechef.com/COOK139C/problems/JGAMES
+
+T = int(input())
+for _ in range(T):
+    X,Y = map(int,input().split())
+    if (X+Y) % 2 == 0:
+        print("Janmansh")
+    else:
+        print("Jay")
