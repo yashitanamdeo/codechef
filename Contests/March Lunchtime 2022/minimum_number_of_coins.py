c3a7b7941361ee1dfde13a88c07cacc8a6af1c62
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-19 22:15:35 +0530

    add Minimum number of coins

--- Contests/March Lunchtime 2022/minimum_number_of_coins.py
+++ Contests/March Lunchtime 2022/minimum_number_of_coins.py
@@ -1,1 +1,16 @@
 # Problem Statement: https://www.codechef.com/LTIME106D/problems/MINCOINS
+
+T = int(input())
+for _ in range(T):
+    X = int(input())
+    if X % 5 != 0:
+        print(-1)
+    else:
+        count = 0
+        while X > 5:
+            X -= 10
+            count += 1
+        if X == 0:
+            print(count)
+        else:
+            print(count+1)
