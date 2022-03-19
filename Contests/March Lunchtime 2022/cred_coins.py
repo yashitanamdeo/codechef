ea8b7028b0aa3df89ba9d27220bf66c720bfd02a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-19 22:14:05 +0530

    add CRED Coins

--- Contests/March Lunchtime 2022/cred_coins.py
+++ Contests/March Lunchtime 2022/cred_coins.py
@@ -1,1 +1,7 @@
 # Problem Statement: https://www.codechef.com/LTIME106D/problems/CREDCOINS
+
+T = int(input())
+for _ in range(T):
+    X,Y = map(int,input().split())
+    product = X*Y
+    print(product//100)
