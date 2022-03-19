e6618edc146d5398859a1ae61414c95b5951df17
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-19 22:18:15 +0530

    add Buy 2 Get 1 Free

--- Contests/March Lunchtime 2022/buy2_get1_free.py
+++ Contests/March Lunchtime 2022/buy2_get1_free.py
@@ -1,1 +1,7 @@
 # Problem Statement: https://www.codechef.com/LTIME106D/problems/SALE2
+
+T = int(input())
+for _ in range(T):
+    N,X = map(int,input().split())
+    count = N - (N//3)
+    print(count*X)
