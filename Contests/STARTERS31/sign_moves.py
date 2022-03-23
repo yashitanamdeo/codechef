2e0d923b03f573124b81e6520b83d96d813fd293
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:52:46 +0530

    add Sign Moves

--- Contests/STARTERS31/sign_moves.py
+++ Contests/STARTERS31/sign_moves.py
@@ -1,1 +1,12 @@
 # Problem Statement: https://www.codechef.com/START31D/problems/SIGNMOVE
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    
+    if N & 1:
+        answer = -1 * ((N + 1) // 2)
+        print(answer)
+    else:
+        answer = N // 2
+        print(answer)
