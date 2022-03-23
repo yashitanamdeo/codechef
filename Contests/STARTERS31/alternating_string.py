5dfd4fe9d0ac814f21304b5a49f07552f54de991
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:51:38 +0530

    add Alternating String

--- Contests/STARTERS31/alternating_string.py
+++ Contests/STARTERS31/alternating_string.py
@@ -1,1 +1,20 @@
 # Problem Statement: https://www.codechef.com/START31D/problems/ALTSTR
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    S = list(input())
+
+    count_of_zero = S.count('0')
+    count_of_one = S.count('1')
+
+    
+    if S == '0'*N or S == '1'*N:
+        print(1)
+        continue
+    num = min(count_of_zero,count_of_one)
+    if count_of_zero == count_of_one:
+        print(count_of_zero + count_of_one)
+        continue
+    else:
+        print((2 * num) + 1)
