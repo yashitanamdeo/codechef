7bc87e778a424320afd91509f47f02ce60280ac7
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:48:04 +0530

    add Cup Finals

--- Contests/STARTERS31/cup_finals.py
+++ Contests/STARTERS31/cup_finals.py
@@ -1,1 +1,10 @@
 # Problem Statement: https://www.codechef.com/START31D/problems/CRICUP
+
+T = int(input())
+for _ in range(T):
+    X,Y,D = map(int,input().split())
+    
+    if abs(X - Y) <= D:
+        print("YES")
+    else:
+        print("NO")
