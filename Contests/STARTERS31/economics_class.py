cb0e778ea4ae78984d88fa13b0492dc8968594d0
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:49:17 +0530

    add Economics Class

--- Contests/STARTERS31/economics_class.py
+++ Contests/STARTERS31/economics_class.py
@@ -1,1 +1,13 @@
 # Problem Statement: https://www.codechef.com/START31D/problems/ECOCLASS
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    S = list(map(int,input().split()))
+    D = list(map(int,input().split()))
+    
+    count = 0
+    for x in range(N):
+        if S[x] == D[x]:
+            count += 1
+    print(count)
