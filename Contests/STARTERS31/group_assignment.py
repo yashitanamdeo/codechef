228b9d4d610a05df71f7317eb02ef125e5d14661
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-03-23 23:50:33 +0530

    add Group Assignment

--- Contests/STARTERS31/group_assignment.py
+++ Contests/STARTERS31/group_assignment.py
@@ -1,1 +1,8 @@
 # Problem Statement: https://www.codechef.com/START31D/problems/GROUPASSGN
+
+T = int(input())
+for _ in range(T):
+    N,X = map(int,input().split())
+    
+    ans = 2*N - X
+    print(ans+1)
